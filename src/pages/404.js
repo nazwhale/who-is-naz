4219574963ee5594d4078c7c51ebd429fecae584
Naz Malik <naz@Nazs-MacBook-Pro.local>
2020-04-21 20:22:10 +0100

    add home button to 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -14,6 +14,9 @@ class NotFoundPage extends React.Component {
         <SEO title="404: Not Found" />
         <h1>Not Found</h1>
         <p>You just hit a route that doesn&#39;t exist...</p>
+        <Link to="/">
+          <Button marginTop="85px">Go Home</Button>
+        </Link>
       </Layout>
     )
   }
