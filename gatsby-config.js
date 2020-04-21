c233998494340611678490b5146730b3b3755483
Naz Malik <naz@Nazs-MacBook-Pro.local>
2020-04-21 17:48:43 +0100

    google analytics tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,8 +63,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // edit below
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-99096863-4`,
       },
     },
     {
